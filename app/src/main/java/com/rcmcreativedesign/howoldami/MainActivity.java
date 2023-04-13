97e7b335323b74ce8b0787119e08f31025ffd768
Robert McDonnell <rcmcd@hotmail.com>
2023-04-12 21:26:49 -0600

    Fixed date conversion bug

--- app/src/main/java/com/rcmcreativedesign/howoldami/MainActivity.java
+++ app/src/main/java/com/rcmcreativedesign/howoldami/MainActivity.java
@@ -1,5 +1,6 @@
 package com.rcmcreativedesign.howoldami;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -8,21 +9,37 @@ import android.widget.CalendarView;
 import android.widget.TextView;
 
 import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Calendar;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
+    private long currentlySelectedLong = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        CalendarView calendar = (CalendarView) findViewById(R.id.calendarView);
+        calendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView calendarView, int year, int month, int day) {
+                Calendar c = Calendar.getInstance();
+                c.set(year, month, day);
+                currentlySelectedLong = c.getTimeInMillis();
+            }
+        });
     }
 
     public void submitButton_click(View view) {
-        CalendarView calendar = (CalendarView) view.findViewById(R.id.calendarView);
-        LocalDate selectedDate = LocalDate.ofEpochDay(calendar.getDate());
+        LocalDate selectedDate = currentlySelectedLong > 0 ?
+                new Date(currentlySelectedLong).toInstant().atZone(ZoneId.systemDefault()).toLocalDate() :
+                LocalDate.now();
         Age calculator = new Age();
         calculator.calculate(selectedDate.toString(), LocalDate.now().toString());
-        TextView textView = (TextView) view.findViewById(R.id.textView);
+        TextView textView = (TextView) findViewById(R.id.textView);
         textView.setText(calculator.getDisplay());
     }
+
+
 }
